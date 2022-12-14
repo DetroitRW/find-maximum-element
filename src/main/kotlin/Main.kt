d36ded34adde348e2f5dc2e6e8fb57e92ad59322
Ilya Brant <detroitib31@gmail.com>
2022-12-14 15:03:59 +0600

    Написал код для создания рандомного числа, создал массив и переменные

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,2 +1,11 @@
+import java.lang.Math.abs
+import java.lang.Math.random
+import java.util.*
+
 fun main(args: Array<String>) {
+    val r = Random(System.nanoTime()).nextInt(-100, 100)
+    val a = 13
+    val n = IntArray(13)
+    var i = 0
+    var j = 0
 
