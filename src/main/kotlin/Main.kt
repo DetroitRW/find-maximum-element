0681b8e06ed47d064c6c0b7e6941dcfde1ebd01b
Ilya Brant <detroitib31@gmail.com>
2022-12-15 12:48:37 +0600

    Разбил всё на функции

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,36 +1,54 @@
 import java.lang.Math.abs
-import java.lang.Math.random
 import java.util.*
 
 fun main(args: Array<String>) {
-    val r = Random(System.nanoTime()).nextInt(-100, 100)
-    val a = 13
-    val n = IntArray(13)
-    var i = 0
-    var j = 0
+    
+    val array = makeArrae()
+    val maxAbsIndex = findMaxAbsIndexInArray(array)
 
-    while (i < a) {
-        val r = Random(System.nanoTime()).nextInt(-100, 100)
-        var f = 1
-        
-        for (j in n) {
-            if (abs(j) == abs(r))
-            f = 0
-            break
-        }
-        if (f == 1) {
-            n[i] = r
-            i += 1
-        }
-    }
-    for (i in 1.. 12) {
-        if (abs(n[i]) > abs(n[j])) {
-            j = i
-        }
-    }
-    for ( item in n ) {
+    printMaxAbsElementInArray(array, maxAbsIndex)
+}
+
+fun randomNumber(): Int {
+    return Random(System.nanoTime()).nextInt(-100, 100)
+}
+
+fun printMaxAbsElementInArray(array: IntArray, MaxAbsInex: Int) {
+    for ( item in array ) {
         print("$item, ")
     }
     println()
-    print("Максимальный по модулю $j элемент равен ${n[j]}")
+    print("Максимальный по модулю $MaxAbsInex элемент равен ${array[MaxAbsInex]}")
+}
+fun findMaxAbsIndexInArray(array: IntArray): Int {
+    var i = 0
+    var j = 0
+
+    for (i in 1.. array.size - 1) {
+        if (abs(array[i]) > abs(array[j])) {
+            j = i
+        }
+    }
+    return j
+}
+
+fun makeArrae(): IntArray {
+    val array = IntArray(13)
+    var i = 0
+
+    while (i < array.size) {
+        val r = randomNumber()
+        var f = 1
+
+        for (j in array) {
+            if (abs(j) == abs(r))
+                f = 0
+            break
+        }
+        if (f == 1) {
+            array[i] = r
+            i += 1
+        }
+    }
+    return array
 }
